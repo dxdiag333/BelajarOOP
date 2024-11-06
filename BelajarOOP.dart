15b5c50625a0ab1489125e826d666f68c5e5253f
dxdiag333 <dmmnpdclid12tf45@gmail.com>
2024-11-06 14:11:48 +0700

    Update BelajarOOP.dart

--- BelajarOOP.dart
+++ BelajarOOP.dart
@@ -1,12 +1,19 @@
-class Siswa {
-	String nama;
+import 'dart:io';
+class Siswa { // class
+	String nama; // property
 	int kelas;
 	List<String> keahlian;
 	int tinggi;
-	Siswa(this.nama, this.kelas, this.keahlian, this.tinggi);
+	Siswa(this.nama, this.kelas, this.keahlian, this.tinggi); // constructor
+  void registerEskulMewarnai() {
+    stdout.write("${"telah memasukan informasi " + nama + ", " + kelas.toString() + " dan " + tinggi.toString()} ke list eskul mewarnai");
+  }
+  void registerEskulMembaca() {
+    stdout.write("${"telah memasukan informasi " + nama + ", " + kelas.toString() + " dan " + tinggi.toString()} ke list eskul membaca");
+  }
 }
 void main() {
-	var siswa_1 = Siswa(
+	var siswa_1 = Siswa( // objek
 		"O. Ranti",
 		12,
 		[
@@ -14,6 +21,7 @@ void main() {
 		],
 		164
 	);
+  siswa_1.registerEskulMembaca();
 	var siswa_2 = Siswa(
 		"A. Zuin",
 		12,
@@ -23,7 +31,7 @@ void main() {
 		171
 	);
 	var siswa_3 = Siswa(
-		"MJK",
+		"MJK", // inputan constructor
 		12,
 		[
 			"matematika"
